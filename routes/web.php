a7033226dd2f2e4c6da05ebbf5b426ddf059cca0
tornike <tornikegerkena02@gmail.com>
2023-11-18 16:22:56 +0400

    ref

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\QuizController;
 use App\Models\Quiz;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,15 @@ use Illuminate\Support\Facades\Route;
 //    return response() -> json($quiz);
 //});
 
-Route::get('/quiz/{quiz?}', [QuizController::class, "createOrUpdate"]);
-Route::get('/', [QuizController::class, 'get'])->name('quiz.list');
+Route::get('/', function() {
+    $quizzes = DB::table('quiz')->where('status', 'active')
+        ->where(function ($query) {$query->whereNotNull('img')
+            ->orWhere(function ($query) {$query->whereNull('img')
+                ->whereNotNull('description');
+            });
+        })
+        ->orderBy('record_date')
+        ->take(8)
+        ->get();
+    return response() -> json($quizzes);
+});
